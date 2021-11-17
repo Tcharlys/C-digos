0ac0a55f4f50ccd47af14ea09e4818343b32e244
Charlys Jerfeson <charlysjerfeson@gmail.com>
2021-11-17 12:49:21 -0300

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main (){
+
+setlocale (LC_ALL,"");
+
+printf("Hello World dentro do Repositório do GitHub\n\n");
+
+    system ("pause");
+    return (0);
+}
